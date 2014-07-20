7f82258f8780997c423c96452d0939f45713377e
oxman <informatique@bouh.org>
2014-07-20 15:01:51 +0200

    Method to retrieve an Entity by id

--- Entity/Repository.php
+++ Entity/Repository.php
@@ -20,6 +20,20 @@ class Repository
     }
 
 
+    public function get($primaryKeyValue)
+    {
+        $className = get_called_class();
+        $entityName = str_replace('Repository', '', $className);
+        $metadata = $this->em->loadMetadata($entityName);
+        $primaryColumn = $metadata->getPrimary()['column'];
+        $sql = 'SELECT * FROM ' . $metadata->getTable() . ' WHERE ' . $primaryColumn . ' = :id LIMIT 1';
+
+        return $this->hydrate(
+            $this->em->doQuery($className, $sql, array('id' => $primaryKeyValue))
+        )->first();
+    }
+
+
     public function query($queryString, $params = array())
     {
         return $this->em->doQuery(get_called_class(), $queryString, $params);
